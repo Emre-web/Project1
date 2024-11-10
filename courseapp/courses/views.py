d8e1bc999a1814317cf85368d99667d645316f6b
Emre-web <Emreyildirimbro@gmail.com>
2024-11-10 20:18:38 +0300

    Creating pagination links

--- courseapp/courses/views.py
+++ courseapp/courses/views.py
@@ -1,55 +1,10 @@
-from django.shortcuts import get_object_or_404, redirect, render
-from django.http import Http404, HttpResponse, HttpResponseNotFound
+from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from datetime import datetime
 from .models import Course, Category
+from django.core.paginator import Paginator
 
-data = {
-    "programlama": "programlama kursları",
-    "web-gelistirme": "web geliştirme kursları",
-    "veritabani": "veritabanı kursları",
-}
 
-db = {
-    "courses": [
-            {
-            "title" : "Python Kursu",
-            "instructor": "Ahmet Mehmet",
-            "description": "Python programlama dilini öğrenmek için bu kursa katılın.",
-            "imageUrl":"1.jpg",
-            "slug": "python-kursu",
-            "date": datetime.now(),
-            "isActive": True,
-            "isUpdated": False
-                    },
-                    {
-            "title" : "React.js Kursu",
-            "instructor": " Mehmet Taha",
-            "description": "React.js Kütüphanesini öğrenmek için bu kursa katılın.",
-            "imageUrl":"2.jpg", 
-            "slug": "React.js-kursu",
-            "date": datetime(2024,11,3),
-            "isActive": True,
-            "isUpdated": True
-                    },
-                    {
-            "title" : "Sql Kursu",
-            "instructor": "Ali Veli",
-            "description": "Sql dilini öğrenmek için bu kursa katılın.",
-            "imageUrl":"3.jpg",
-            "slug": "Sql-kursu",
-            "date": datetime(2024,11,3),
-            "isActive": True,
-            "isUpdated": True
-                    },
-    ],
-        "categories": [
-       { "id": 1, "name": "programlama", "slug": "programlama" },
-       { "id": 2, "name": "web-gelistirme", "slug": "web-gelistirme"},
-       { "id": 3, "name": "veritabani", "slug": "veritabani" },
-          ]
-    
-}
 def index(request):
     kurslar = Course.objects.filter(isActive=True)
     kategoriler = Category.objects.all()
@@ -76,11 +31,17 @@ def details(request, slug):
 
      
 def getCoursesByCategory(request, slug):
-    kurslar = Course.objects.filter(categories__slug=slug, isActive=True)
-    kutegoriler = Category.objects.all()
+    kurslar = Course.objects.filter(categories__slug=slug, isActive=True).order_by('-date')
+    kategoriler = Category.objects.all()
+
+
+    paginator = Paginator(kurslar, 1)
+    page_number = request.GET.get('page',1) 
+    page_object = paginator.page(page_number)
+
 
     return render(request, 'coursespages/index.html', {
-        'categories': kutegoriler,
-        'courses': kurslar,
+        'categories': kategoriler,
+        'page_object': page_object,
         'seciliCategory': slug
     })
