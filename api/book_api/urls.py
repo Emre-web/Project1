94a5695f6a629a474dc5f0dcc0722e772781da7d
Emre-web <Emreyildirimbro@gmail.com>
2024-12-16 03:15:19 +0300

    # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # # On branch main # Your branch is ahead of 'origin/main' by 2 commits. #   (use "git push" to publish your local commits) # # Changes to be committed:

--- api/book_api/urls.py
+++ api/book_api/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.books),
     path('<int:id>/', views.book),
-    path('create/', views.book_create),
-    path('list/', views.book_list),
-]
+] 
+
+
+#localhost/books => GET, POST
+#localhost/books/2 => GET, PUT, DELETE
--- api/book_api/views.py
+++ api/book_api/views.py
@@ -6,29 +6,39 @@ from book_api.models import Book
 from book_api.serializer import BookSerializer
 from rest_framework import status
 
-@api_view(['GET'])   
+@api_view(['GET','POST'])   
 
-def book_list(request):
-    books = Book.objects.all()
-    serializer = BookSerializer(books,many=True)
-    return Response(serializer.data)
+def books(request):
+    if request.method == 'GET':
+        books = Book.objects.all()
+        serializer = BookSerializer(books,many=True)
+        return Response(serializer.data)
+    if request.method == 'POST':
+        serializer = BookSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        else: 
+            return Response(serializer.errors)    
+        
  
-@api_view(['GET'])
+@api_view(['GET','PUT','DELETE'])
 def book(request,id):
     try:
          book = Book.objects.get(pk = id)
-         serializer = BookSerializer(book)
-         return Response(serializer.data)
     except: 
         return Response({"error": "Eşleşen bir kayıt bulunamadı."}, status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'GET':
+         serializer = BookSerializer(book)
+         return Response(serializer.data)
+    elif request.method == 'PUT':
+         serializer =  BookSerializer(book, data = request.data)
+         if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+         return Response(serializer.errors)
 
-@api_view(['POST'])
-def book_create(request):
-    serializer = BookSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data)
-    else: 
-        return Response(serializer.errors)
-    
-    
+    elif request.method == 'DELETE':
+         book.delete()
+         return Response(status=status.HTTP_204_NO_CONTENT)
